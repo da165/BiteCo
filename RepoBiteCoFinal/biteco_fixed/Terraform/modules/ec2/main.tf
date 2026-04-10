93ed98a7fec359ff977d0ef35bc47bb2817b281b
David Ortiz Alfaro <da.ortiza1@uniandes.edu.co>
2026-04-10 06:50:39 -0500

    Fix syntax for user_data variable declaration

--- RepoBiteCoFinal/biteco_fixed/Terraform/modules/ec2/main.tf
+++ RepoBiteCoFinal/biteco_fixed/Terraform/modules/ec2/main.tf
@@ -7,7 +7,7 @@ variable "instance_type"     { type = string }
 variable "key_name"          { type = string }
 variable "security_group_id" { type = string }
 variable "project"           { type = string }
-variable "user_data"         { type = string; default = "" }
+variable "user_data"         { type = string default = "" }
 
 resource "aws_instance" "this" {
   ami                         = var.ami
